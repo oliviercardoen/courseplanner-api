5d1752f40abca7469eaa31216be0eb384579a702
Olivier Cardoen <olivier.cardoen@octopix.be>
2014-11-07 06:15:36 +0100

    Create a generic Controller class to abstract all controller logic and format child classes.

--- src/BaseModule/Controller/CourseController.php
+++ src/BaseModule/Controller/CourseController.php
@@ -1,69 +1,8 @@
 <?php
 namespace CoursePlanner\BaseModule\Controller;
 
-use Octopix\Selene\Mvc\Controller\Rest\RestController;
-use CoursePlanner\BaseModule\Model\Course;
-use Octopix\Selene\Mvc\Model\ModelArray;
+class CourseController extends Controller {
 
-class CourseController extends RestController {
-
-	/**
-	 *
-	 */
-	public function index()
-	{
-		$this->render( ModelArray::each( Course::all() ) );
-	}
-
-	/**
-	 * @param $id
-	 * @return mixed|void
-	 */
-	public function show($id)
-	{
-		$this->render( ModelArray::one( Course::find( (int) $id ) ) );
-	}
-
-	/**
-	 *
-	 */
-	public function create()
-	{
-		$raw  = $this->request()->getBody();
-		$model = new Course();
-		$model->hydrate( json_decode( $raw, true ) );
-		$model->save();
-		$this->render( $model->as_array() );
-	}
-
-	/**
-	 * @param $id
-	 * @return mixed|void
-	 */
-	public function update($id)
-	{
-		$raw  = $this->request()->getBody();
-		$model = Course::find( (int) $id );
-		if ( empty( $model ) ) {
-			$model = new Course();
-		}
-		$model->hydrate( json_decode( $raw, true ) );
-		$model->save();
-		$this->render( $model->as_array() );
-	}
-
-	/**
-	 * @param $id
-	 * @return mixed|void
-	 */
-	public function delete($id)
-	{
-		$model = Course::find( (int) $id );
-		if ( !empty( $model ) ) {
-			// TODO: Breaks because related to curriculum (SQL Constraint).
-			$model->delete();
-			$this->render( $model->as_array() );
-		}
-	}
+	private static $_model_class = 'CoursePlanner\BaseModule\Model\Course';
 
 }
